2e8cda5e1221687a5cca60fb1b7dce1771938e64
Chad Dundas Smith <chaddundassmith@Chads-MacBook-Pro.local>
2022-02-05 18:18:24 -0700

    first api call

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,49 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
+
+const PROVIDERS_ENDPOINT = 'http://localhost:3000/providers';
+//TODO inteface for providers so useState is not any
 function App() {
+  const [providers, setProviders] = useState<any[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState();
+  useEffect(() => {
+    // this block will run on mount
+    
+    const fetchProviders = async () => {
+      setIsLoading(true);
+      try {
+        const response = await axios.get(PROVIDERS_ENDPOINT);
+        setProviders(response.data);
+        setIsLoading(false);
+      } catch (error: any) {
+        setError(error);
+      }
+      
+    }
+
+    fetchProviders();
+  }, []);
+
+  if (error) {
+    return <div>Sorry something went wrong!</div>
+  }
+
+  if (isLoading) {
+    <img src={logo} className="App-logo" alt="logo" />
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+          {providers.map((provider) => (
+          <div>
+            <div>{provider.name}</div><div>{provider.id}</div>
+          </div> 
+          ))}        
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
@@ -16,7 +53,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Basic Login
+          
         </a>
       </header>
     </div>
